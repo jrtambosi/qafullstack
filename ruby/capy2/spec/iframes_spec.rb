79b78af92ef13725a84d4ef56c3c5e8691f3c7a6
jrtambosi <junior.tambosi@gmail.com>
2019-04-11 06:22:15 -0300

    iframebomeruim

---
+++ ruby/capy2/spec/iframes_spec.rb
@@ -0,0 +1,49 @@
+describe 'Ifremes' , :dc do
+
+    describe ' bom' , :nice_bom do
+        before(:each) do
+
+            visit 'https://training-wheels-protocol.herokuapp.com/nice_iframe'
+        end
+    
+        it 'Adicionar no carrinho'  do
+            
+            within_frame('burgerId') do
+            
+                produto = find('.menu-item-info-box', text: 'REFRIGERANTE')
+                produto.find('a').click
+                expect(find('#cart')).to have_content 'R$ 4,50'
+
+                sleep 5
+
+            
+            end
+
+        end
+    
+    end
+    
+    describe 'IFRAME RUIM' ,:ruim do
+        before(:each) do
+
+            visit 'https://training-wheels-protocol.herokuapp.com/bad_iframe'
+        end
+
+        it ' carrinho deve estar vazio ' do 
+
+            script = '$(".box-iframe").attr("id", "tempId");'
+
+            page.execute_script(script)
+
+            within_frame('tempId') do
+            
+                expect(find('#cart')).to have_content 'Seu carrinho está vazio!'
+                sleep 3
+            
+            end
+        end
+
+    end
+
+
+end
