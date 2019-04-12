fef99d01e87f1a5c9b3a714dd9988b948ab488d2
jrtambosi <junior.tambosi@gmail.com>
2019-04-11 21:24:26 -0300

    iddinamico

---
+++ ruby/capy2/spec/id_dinamico_spec.rb
@@ -0,0 +1,21 @@
+describe 'IDS Dinamicos' , :Din do
+
+    before(:each) do
+
+        visit'https://training-wheels-protocol.herokuapp.com/access'
+    end
+
+    it 'cadastro'  do
+
+        find('input[id$=UsernameInput]').set 'fernando'
+        find('input[id^=PasswordInput]').set  '123456'
+        find('a[id*=GetStartedButton]').click
+        
+        expect(page).to have_content 'Dados enviados. Aguarde aprovação do seu cadastro!'
+        sleep 3
+
+    
+    end
+
+
+end
