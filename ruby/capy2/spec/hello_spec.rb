ed32e71187827881cbe8d3295e7efb8d4876c790
jrtambosi <junior.tambosi@gmail.com>
2019-04-14 09:25:54 -0300

    tamanhoTela

--- ruby/capy2/spec/hello_spec.rb
+++ ruby/capy2/spec/hello_spec.rb
@@ -1,5 +1,8 @@
-describe ' primeiro script' do
+describe ' primeiro script', :heloo do
 
+    
+
+    
     it 'visitar site' do
 
     visit '/'
--- ruby/capy2/spec/spec_helper.rb
+++ ruby/capy2/spec/spec_helper.rb
@@ -21,6 +21,11 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   config.include Capybara::DSL 
+  
+  config.before(:example) do
+   page.current_window.resize_to(1280,800)
+
+  end
 
 end
 
@@ -29,6 +34,7 @@ Capybara.configure do |config|
  # onfig.default_driver = :selenium
   config.default_max_wait_time = 5
   config.app_host = 'https://training-wheels-protocol.herokuapp.com'
+  
 
 
 end
